86a432f9acf9c378dcae84c1a4632f37e0891cdc
williamdotyang <williamdotyang@gmail.com>
2015-12-17 23:24:03 -0600

    no change

--- 114/stackRecursive.py
+++ 114/stackRecursive.py
@@ -1,14 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 ## push the node vals in a stack, with right-most in the bottom
 def pushNodesInStack(root, stack):
-        if (root == None):
-            return stack
-        if not root.right == None:
-            stack = pushNodesInStack(root.right, stack)
-        if not root.left == None:
-            stack = pushNodesInStack(root.left, stack)
-        stack.append(root.val)
-        root = None
+    if (root == None):
         return stack
+    if not root.right == None:
+        stack = pushNodesInStack(root.right, stack)
+    if not root.left == None:
+        stack = pushNodesInStack(root.left, stack)
+    stack.append(root.val)
+    return stack
 
 ## recursively set the right val all the way up to root
 def setRight(root, stack):
