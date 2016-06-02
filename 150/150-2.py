780ab9e697818a97c3ea93a23c351278f47b00a8
williamdotyang <williamdotyang@gmail.com>
2016-06-02 13:46:40 +0800

    textbook implementation, still something wrong.

---
+++ 150/150-2.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    operators = ["+", "-", "*", "/"]
+    
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for token in tokens:
+            if token not in Solution.operators:
+                stack.append(int(token))
+            else:
+                stack.append(self.evalOperation(token, stack))
+        if len(stack) == 1:
+            return stack.pop()
+        else:
+            print "too many tokens"
+
+    def evalOperation(self, token, stack):
+        if token == "+":
+            operand1 = stack.pop()
+            operand2 = stack.pop()
+            return operand2 + operand1
+        elif token == "-":
+            operand1 = stack.pop()
+            operand2 = stack.pop()
+            return operand2 - operand1
+        elif token == "*":
+            operand1 = stack.pop()
+            operand2 = stack.pop()
+            return operand2 * operand1
+        elif token == "/":
+            operand1 = stack.pop()
+            operand2 = stack.pop()
+            return operand2 / operand1
+        else:
+            print "unrecognized operator"
